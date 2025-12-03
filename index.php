9ada6933289dbb535a27c3eb4225816f2aac330b
Fernanda Maressa <fernandamaressa04@gmail.com>
2025-12-03 09:24:44 -0300

    Add index.php na raiz para rodar o indice

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Índice dos Exercícios</title>
+</head>
+<body>
+    <h1>Lista de Exercícios</h1>
+
+    <ul>
+        <li><a href="exercicio1/">Exercício 1 - Verificação de Idade</a></li>
+    </ul>
+
+</body>
+</html>
