c96aece2c2ffb4035f174f5e3927e6eca70ce915
Fernanda Maressa <fernandamaressa04@gmail.com>
2026-01-09 10:37:50 -0300

    alterando logica do back para nao aceitar valor menor ou igual a 0

--- exercicio4/processar.php
+++ exercicio4/processar.php
@@ -1,42 +1,74 @@
 <?php
-$atletas = [
-    $_POST['nome1'] => $_POST['tempo1'],
-    $_POST['nome2'] => $_POST['tempo2'],
-    $_POST['nome3'] => $_POST['tempo3']
-];
+$n1 = $_POST['nome1'] ?? 'Atleta 1';
+$t1 = floatval($_POST['tempo1'] ?? 0);
+$n2 = $_POST['nome2'] ?? 'Atleta 2';
+$t2 = floatval($_POST['tempo2'] ?? 0);
+$n3 = $_POST['nome3'] ?? 'Atleta 3';
+$t3 = floatval($_POST['tempo3'] ?? 0);
 
-asort($atletas);
+if ($t1 <= 0 || $t2 <= 0 || $t3 <= 0) {
+    die("Erro: Todos os atletas devem ter um tempo maior que zero. <a href='index.php'>Voltar</a>");
+}
+
+if ($t1 < $t2 && $t1 < $t3) {
+    $primNome = $n1; $primTempo = $t1;
+    if ($t2 < $t3) {
+        $segNome = $n2; $segTempo = $t2;
+        $terNome = $n3; $terTempo = $t3;
+    } else {
+        $segNome = $n3; $segTempo = $t3;
+        $terNome = $n2; $terTempo = $t2;
+    }
+} elseif ($t2 < $t1 && $t2 < $t3) {
+    $primNome = $n2; $primTempo = $t2;
+    if ($t1 < $t3) {
+        $segNome = $n1; $segTempo = $t1;
+        $terNome = $n3; $terTempo = $t3;
+    } else {
+        $segNome = $n3; $segTempo = $t3;
+        $terNome = $n1; $terTempo = $t1;
+    }
+} else {
+    $primNome = $n3; $primTempo = $t3;
+    if ($t1 < $t2) {
+        $segNome = $n1; $segTempo = $t1;
+        $terNome = $n2; $terTempo = $t2;
+    } else {
+        $segNome = $n2; $segTempo = $t2;
+        $terNome = $n1; $terTempo = $t1;
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Pódio da Corrida</title>
+    <title>Resultado - Pódio</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<div class="podio-container">
-    <h1>PÓDIO DA CORRIDA 🏆</h1>
+<div class="container">
+    <h1>🏆 PÓDIO FINAL 🏆</h1>
 
-    <div class="podio">
-        <?php
-        $posicao = 1;
-        foreach ($atletas as $nome => $tempo) {
-            echo "
-            <div class='lugar lugar-$posicao'>
-                <strong>{$posicao}º Lugar</strong>
-                <p>$nome</p>
-                <span>$tempo segundos</span>
-            </div>";
-            $posicao++;
-        }
-        ?>
+    <div class="atleta">
+        <label>🥇 1º LUGAR</label>
+        <p><strong><?= $primNome ?></strong> - <?= $primTempo ?> segundos</p>
     </div>
 
-    <a href="index.php">
-        <button>Voltar</button>
+    <div class="atleta">
+        <label>🥈 2º LUGAR</label>
+        <p><strong><?= $segNome ?></strong> - <?= $segTempo ?> segundos</p>
+    </div>
+
+    <div class="atleta">
+        <label>🥉 3º LUGAR</label>
+        <p><strong><?= $terNome ?></strong> - <?= $terTempo ?> segundos</p>
+    </div>
+
+    <a href="index.php" style="text-decoration: none;">
+        <button type="button" style="width: 100%;">VOLTAR</button>
     </a>
 </div>
 
