1e846617ceb223beabb6b83a1c2b5a8f8e1c00c4
Fernanda Maressa <fernandamaressa04@gmail.com>
2026-02-11 09:56:25 -0300

    Mudanças necessarias para receber o back

--- exercicio8/index.php
+++ exercicio8/index.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contador Regressivo</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
 <div class="container">
-    <h1>Contador Regressivo</h1>
-    
+    <h1>Contagem Regressiva</h1>
+
     <form action="processar.php" method="POST">
-        <label for="numero">Número Inicial da Contagem:</label>
-        <input type="number" id="numero" name="numero" min="5" max="50" required placeholder="Digite entre 5 e 50">
-        
-        <button type="submit">Iniciar Contagem Regressiva</button>
+        <label for="numero">Número inicial da Contagem:</label>
+        <input type="number" name="numero" id="numero" min="5" max="50" required placeholder="Digite entre 5 e 50">
+
+        <button type="submit">Iniciar Contagem</button>
     </form>
 </div>
+
 </body>
 </html>
-
