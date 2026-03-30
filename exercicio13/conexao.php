679d10c547265ccaff8b5f080031e03982fccf34
Fernanda Maressa <fernandamaressa04@gmail.com>
2026-03-30 18:57:22 -0300

    feat: configura conexao para novos_titans_db

---
+++ exercicio13/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+$host    = 'db';
+$dbname  = 'novos_titans_db';
+$user    = 'root';
+$pass    = 'root';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+$opcoes = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $opcoes);
+} catch (PDOException $e) {
+    die('Erro de conexão com o banco de dados: ' . $e->getMessage());
+}
